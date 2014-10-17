639285011efbb3442a31400bbe1f9c5ff7b547ed
V David Zvenyach <vdavez@gmail.com>
2014-10-17 15:34:26 -0400

    ready for showtime

--- raisethebar/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app.models import *
-
-# Register your models here.
-admin.site.register(Firm)
-admin.site.register(Contact)
-admin.site.register(Note)
-admin.site.register(Reminder)
