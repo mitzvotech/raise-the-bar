dc093cbcf67db86772c5126bb1038ab155797a5e
V David Zvenyach <vdavez@gmail.com>
2014-10-17 20:42:14 -0400

    settings fix

--- raisethebar/settings.py
+++ raisethebar/settings.py
@@ -86,12 +86,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(PROJECT_PATH, 'staticfiles'),
 )
 
 import dj_database_url
